9a1476387a9148760aa42f3fc9c3fe2e2090cf97
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2013-08-30 08:27:12 +0200

    Add skeleton for new UI

---
+++ src/nl/simbits/rambler/holo/MainActivity.java
@@ -0,0 +1,10 @@
+package nl.simbits.rambler.holo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
